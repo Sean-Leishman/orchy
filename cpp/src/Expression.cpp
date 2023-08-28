8e421760ffc2800ca887bcea4feb4d395da1b196
Sean Leishman <leishmansean@gmail.com>
2023-08-29 00:17:20 +0100

    Generate expression class .cpp

---
+++ cpp/src/Expression.cpp
@@ -0,0 +1,10 @@
+class Binary;
+class Grouping;
+class Literal;
+class Unary;
+class Visitor;class Expression{
+template <typename T> T accept(Visitor<T>* visitor);};class Binary : public Expression{
+Expression left;Token op;Expression right;Binary(Expression left,Token op,Expression right) :left(left),op(op),right(right){}};class Grouping : public Expression{
+Expression expression;Grouping(Expression expression) :expression(expression){}};class Literal : public Expression{
+Object value;Literal(Object value) :value(value){}};class Unary : public Expression{
+Token op;Expression right;Unary(Token op,Expression right) :op(op),right(right){}};template <typename T> class Visitor {virtual T visitBinary_Expression(Binary);virtual T visitGrouping_Expression(Grouping);virtual T visitLiteral_Expression(Literal);virtual T visitUnary_Expression(Unary);};
